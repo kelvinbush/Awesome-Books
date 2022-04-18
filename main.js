3ae4634e6e2ccdcd746ae0d8bab429a32f3f8f31
Kelvin Bush <kelybush@gmail.com>
2022-04-18 10:39:52 +0100

    Render books to page via js

---
+++ main.js
@@ -0,0 +1,23 @@
+const books = [
+  {
+    title: "The Design of EveryDay Things",
+    author: "Don Norman",
+    id: 1
+  },
+  {
+    title: "The Most Human Human",
+    author: "Brian Christian",
+    id: 2
+  }
+]
+
+
+const bookListSection = document.querySelector('#book-list')
+function renderBookList(bookList) {
+  bookListSection.innerHTML = bookList.map((book) => {
+    return `<p class="title">${book.title}</p>
+            <p>${book.author}</p>
+            <button data-id=${book.id} class="remove">Remove</button>
+            <hr>`
+  }).join('')
+}
