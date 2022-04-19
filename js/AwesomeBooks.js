a28ad324291893d24eaf5d47d08d0af9a4e8b049
Kelvin Bush <kelybush@gmail.com>
2022-04-19 15:56:32 +0100

    Set up book class

---
+++ js/AwesomeBooks.js
@@ -0,0 +1,43 @@
+import Book from './Book.js';
+
+class AwesomeBooks {
+  constructor() {
+    this.books = [];
+  }
+
+  addBook(title, author) {
+    const id = this.books.length + 1;
+    const book = new Book(title, author, id);
+    this.books.push(book);
+    this.#save();
+  }
+
+  #addBooks(books) {
+    books.forEach((book) => {
+      this.addBook(book.title, book.author);
+    });
+  }
+
+
+  getBooks() {
+    return this.books;
+  }
+
+  #save() {
+    localStorage.setItem('books', JSON.stringify(this.books));
+  }
+
+  load() {
+    const books = JSON.parse(localStorage.getItem('books'));
+    if (books) {
+      this.#addBooks(books);
+    }
+  }
+
+  deleteBook(id) {
+    this.books = this.books.filter((book) => book.id !== id);
+    this.#save();
+  }
+}
+
+export default AwesomeBooks;
